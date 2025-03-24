870bf74b032ddf43e62f9fdcd029ca5e5ef248cc
mgm136044 <mkm0530@koreatech.ac.kr>
2025-03-24 18:27:41 +0900

    feat:입력 구조 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,17 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
-import java.util.SplittableRandom;
 
 public class Application {
     public static void main(String[] args) {
         java.io.Console c = System.console();
+        String CarNameInput = c.readLine("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 로 구분하여 입력)");
+        String[] CarName = CarNameInput.split(",");
+        String TryInput = c.readLine("시도할 회수는 몇 회인가요?");
+        int HowManyTryInput = Integer.parseInt(TryInput);
 
-        String carNameinput = c.readLine("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 로 구분하여 입력)");
-        String[] carName = carNameinput.split(",");
-        String carRealName = c.readLine(",");
-        SplittableRandom r = new SplittableRandom();
-        int carPosition = r.nextInt(carName.length);
-
+        int RandomNumber = Randoms.pickNumberInRange(0,9);
 
 
     }
