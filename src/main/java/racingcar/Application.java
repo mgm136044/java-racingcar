1cde5632fe5e024d586365651090b86d83d98527
mgm136044 <mkm0530@koreatech.ac.kr>
2025-03-24 18:12:31 +0900

    feat:입력 구조 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.SplittableRandom;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        java.io.Console c = System.console();
+
+        String carNameinput = c.readLine("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 로 구분하여 입력)");
+        String[] carName = carNameinput.split(",");
+        String carRealName = c.readLine(",");
+        SplittableRandom r = new SplittableRandom();
+        int carPosition = r.nextInt(carName.length);
+
+
+
     }
 }
