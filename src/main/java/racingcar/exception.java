c58d356395841b25cad22a592f27c0ec27b2f425
mgm136044 <mkm0530@koreatech.ac.kr>
2025-03-31 14:24:20 +0900

    feat: exception 파일 내 예외 처리 함수 2가지 메서드로 분할

--- src/main/java/racingcar/exception.java
+++ src/main/java/racingcar/exception.java
@@ -1,19 +1,27 @@
 package racingcar;
 
 public class exception {
-    public static void validateInputs(String carNameInput, String tryInput) { // 예외 처리 함수
-        if (carNameInput == null || carNameInput.trim().isEmpty()) { // 이름 없는 경우
+    public static void validateInputs(String carNameInput, String tryInput) {
+        validateCarNames(carNameInput);
+        validateTryCount(tryInput);
+    }
+
+    public static void validateCarNames(String carNameInput) {
+        if (carNameInput == null || carNameInput.trim().isEmpty()) {
             throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다.");
         }
         String[] names = carNameInput.split(",");
         for (String name : names) {
-            if (name.trim().isEmpty()) {
+            if (name.trim().isEmpty()) { // 빈 이름 허용x
                 throw new IllegalArgumentException("빈 자동차 이름이 존재합니다.");
             }
             if (name.trim().length() > 5) { // 최대 5자 제한
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
         }
+    }
+
+    public static void validateTryCount(String tryInput) {
         try {
             int count = Integer.parseInt(tryInput);
             if (count < 1) {
